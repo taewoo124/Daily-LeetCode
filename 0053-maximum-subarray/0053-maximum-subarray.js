bd32439ab7ac01d6745a82ae66f37f136e21d842
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-06-22 14:01:26 +0900

    Time: 100 ms (6.26%), Space: 55.6 MB (5.18%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    const newArr = [];
+
+    for (let i = 0; i < nums.length; i += 1) {
+        newArr[i] = nums[i];
+
+      if (newArr[i] < newArr[i - 1] + nums[i]) {
+          newArr[i] = newArr[i - 1] + nums[i];
+      }
+    }
+
+    return Math.max(...newArr);
+};
