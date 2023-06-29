096374ea317d04f14188bc5731456624e7e911c0
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-06-29 21:18:46 +0900

    Time: 95 ms (16.80%), Space: 51.7 MB (40.66%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    let minPrice = prices[0];
+
+    for (let i = 1; i < prices.length; i += 1) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+};
