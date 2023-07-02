6c2969137d9af2c7c9807bb1dc84d0fcea85cb9c
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-07-02 15:06:35 +0900

    Time: 78 ms (74.14%), Space: 44.5 MB (55.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,8 +4,25 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    const sortedS = [...s].sort().join();
-    const sortedT = [...t].sort().join();
-
-    return sortedS === sortedT
+    if (s.length !== t.length) return false;
+    
+    const hashMap = {};
+    
+    for (const char of s) {
+        if (hashMap[char]) {
+            hashMap[char] += 1;
+        } else {
+            hashMap[char] = 1;
+        }
+    }
+    
+    for (const char of t) {
+        if (hashMap[char]) {
+            hashMap[char] -= 1;
+        } else {
+            return false;
+        }
+    }
+    
+    return true;
 };
