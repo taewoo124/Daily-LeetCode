1dc98a659f4c13ea1742e7ce8dd3745b09f14589
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-07-03 20:05:55 +0900

    Time: 73 ms (14.73%), Space: 45.1 MB (58.04%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+
+
+    while (start <= end) {
+      const middle = Math.floor((start + end) / 2);
+
+      if (nums[middle] === target) {
+          return middle
+      } else if (nums[middle] < target) {
+          start = middle + 1;
+      } else {
+          end = middle - 1;
+      }
+    }
+
+    return -1;
+};
