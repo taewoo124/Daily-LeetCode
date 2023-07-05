0c1aed71df6afb151a83dd217200dc934f5f201e
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-07-05 21:35:33 +0900

    Time: 106 ms (79.50%), Space: 46.1 MB (68.59%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let dp = new Array(amount + 1).fill(Infinity);
+    dp[0] = 0;
+
+    for (let coin of coins) {
+      for (let i = coin; i <= amount; i += 1) {
+          dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
+    }
+
+    return dp[amount] !== Infinity ? dp[amount] : -1;
+};
