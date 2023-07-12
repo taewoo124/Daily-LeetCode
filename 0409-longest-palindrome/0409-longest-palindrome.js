883f8d88a6c9cb396de011f3063e17c23d9fadad
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-07-12 17:47:18 +0900

    Time: 66 ms (59.40%), Space: 44.6 MB (29.40%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -3,26 +3,28 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    let freqCount = {};
-    let oddCount = 0;
-
-    for (let char of s) {
-        if (freqCount[char] === undefined) {
-            freqCount[char] = 1;
-        } else {
-            freqCount[char]++;
-        }
-
-        if (freqCount[char] % 2 === 1) {
-            oddCount++;
-        } else {
-            oddCount--;
-        }
-    }
-
-    if (oddCount > 1) {
-        return s.length - (oddCount - 1);
+  const hashMap = {};
+  let oddCount = 0;
+  
+  for (const char of s) {
+    if (!hashMap[char]) {
+      hashMap[char] = 1;
     } else {
-        return s.length;
+      hashMap[char] += 1;
     }
+    
+    if (hashMap[char] % 2 === 1) {
+      oddCount += 1;
+    } else {
+      oddCount -= 1;
+    }
+  }
+  
+  if (oddCount > 1) {
+
+    return s.length - (oddCount - 1);
+  } else {
+
+    return s.length;
+  }
 };
