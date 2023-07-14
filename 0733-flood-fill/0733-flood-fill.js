6c27aab1d9fb37c985133227971b039b8dd4586e
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-07-14 10:14:59 +0900

    Time: 85 ms (16.53%), Space: 45.1 MB (13.56%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+  if (image[sr][sc] === color) return image;
+  
+  let startColor = image[sr][sc];
+  const queue = [[sr, sc]];
+  
+  while (queue.length > 0) {
+    const currentPixel = queue.shift();
+    const [i , j] = currentPixel;
+    
+    if (image[i][j] === startColor) {
+      image[i][j] = color;
+      
+      if (i > 0) queue.push([i - 1, j]);
+      if (j > 0) queue.push([i, j - 1]);
+      if (i < image.length - 1) queue.push([i + 1, j]);
+      if (j < image[0].length - 1) queue.push([i, j + 1]);
+    }
+  }
+  
+  return image;
+};
