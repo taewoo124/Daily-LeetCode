1aad00bd5060f0eeafdf5b4e57fba51c8cbad858
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-07-20 14:30:07 +0900

    Time: 5951 ms (6.56%), Space: 66.6 MB (54.99%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -2,18 +2,24 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    const result = new Array(temperatures.length).fill(0);
+const dailyTemperatures = function (temperatures) {
+  const result = new Array(temperatures.length).fill(0);
 
-    for (let i = 0; i < temperatures.length; i += 1) {
-        for (let j = i + 1; j < temperatures.length; j += 1) {
+  for (let i = 0; i < temperatures.length; i += 1) {
+    let count = 0;
 
-            if (temperatures[j] > temperatures[i]) {
-                result[i] = j - i;
-                break;
-            }
-        }
+    for (let j = i + 1; j < temperatures.length; j += 1) {
+      count += 1;
+
+      if (temperatures[i] < temperatures[j]) {
+        result[i] = count;
+        break;
+      }
+
+      if (j === temperatures.length - 1) result[i] = 0;
     }
+  }
 
-    return result;
+  return result;
 };
+
