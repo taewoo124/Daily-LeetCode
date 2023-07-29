02098e47232ad5aebc21d2c9cdb50e5d07157e2d
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-07-29 13:59:07 +0900

    Time: 86 ms (98.74%), Space: 54.8 MB (44.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,23 +2,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const productExceptSelf = function(nums) {
-    const length = nums.length;
-    const result = new Array(length).fill(1);
-
-    let productBefore = 1;
-
-    for (let i = 0; i < length; i += 1) {
-        result[i] *= productBefore;
-        productBefore *= nums[i];
-    }
-
-    let productAfter = 1;
-
-    for (let i = length - 1; i >= 0; i -= 1) {
-        result[i] *= productAfter;
-        productAfter *= nums[i];
-    }
-
-    return result;
+var productExceptSelf = function(nums) {
+  const answer = new Array(nums.length);
+  answer.fill(1);
+  
+  let left = 1;
+  
+  for (let i = 0; i < nums.length; i += 1) {
+    answer[i] *= left;
+    left *= nums[i];
+  }
+  
+  let right = 1;
+  
+  for (let i = nums.length - 1; i >= 0; i -= 1) {
+    answer[i] *= right;
+    right *= nums[i]
+  }
+  
+  return answer;
 };
