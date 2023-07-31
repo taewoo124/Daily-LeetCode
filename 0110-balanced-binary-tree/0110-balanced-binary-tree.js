50f6138cd075d48970bcdc3b8bc41b53fa18cda7
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-07-31 22:02:18 +0900

    Time: 57 ms (98.43%), Space: 46.6 MB (93.45%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    return height(root) !== -1;
+};
+
+function height(node) {
+    if (node === null) {
+        return 0;
+    }
+    const leftHeight = height(node.left);
+    const rightHeight = height(node.right);
+    if (leftHeight === -1 || rightHeight === -1 || Math.abs(leftHeight - rightHeight) > 1) {
+        return -1;
+    } else {
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
