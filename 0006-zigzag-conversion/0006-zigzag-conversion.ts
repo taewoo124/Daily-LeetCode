5e937bf323338413452d7638cf45e8deac317955
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-08-19 20:15:07 +0900

    Time: 76 ms (76.47%), Space: 46.2 MB (97.06%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.ts
+++ 0006-zigzag-conversion/0006-zigzag-conversion.ts
@@ -1,20 +1,22 @@
 function convert(s: string, numRows: number): string {
   if (numRows === 1) return s;
-  
-  const rows: string[] = Array(numRows).fill("");
-  
-  let count: number = 0;
-  let direction: boolean = true;
-  
+
+  const rows: string[] = Array(numRows).fill(""); // 2차원 배열 형식처럼 각 행 마다 빈 문자열 설정
+
+  let count: number = 0; // 현재 row
+  let direction: boolean = true; // 진행 방향
+
   for (const char of s) {
-    rows[count] += char;
-    
+    // 모든 문자열 순회
+    rows[count] += char; // 차례로 문자 입력
+
     if (count === 0 || count === numRows - 1) {
-      direction = !direction;
+      direction = !direction; // 첫 줄이거나 마지막줄에서 방향 전환
     }
-    
-    count += direction ? -1 : 1;
+
+    count += direction ? -1 : 1; // 방항 변경
   }
-  
-  return rows.join("");
-};
+  const result = rows.join("");
+
+  return result;
+}
