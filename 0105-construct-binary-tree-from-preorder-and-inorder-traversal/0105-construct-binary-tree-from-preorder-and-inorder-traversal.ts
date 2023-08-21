bb77d3c04cae78db74fb67e0194f7ca904404326
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-08-21 13:20:18 +0900

    Time: 124 ms (42.63%), Space: 138.6 MB (18.91%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -15,10 +15,10 @@
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
   if (preorder.length === 0 || inorder.length === 0) return null;
 
-  const rootVal = preorder[0];
-  const root = new TreeNode(rootVal);
+  const rootVal: number = preorder[0];
+  const root: TreeNode = new TreeNode(rootVal);
 
-  const inorderIndex = inorder.indexOf(rootVal);
+  const inorderIndex: number = inorder.indexOf(rootVal);
 
   root.left = buildTree(preorder.slice(1, inorderIndex + 1), inorder.slice(0, inorderIndex));
   root.right = buildTree(preorder.slice(inorderIndex + 1), inorder.slice(inorderIndex + 1));
