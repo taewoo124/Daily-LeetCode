7b8b8a26dbdc2d1607be11ec41d7c8521b524784
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-08-21 11:37:15 +0900

    Time: 124 ms (42.63%), Space: 138.2 MB (33.01%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  if (preorder.length === 0 || inorder.length === 0) return null;
+
+  const rootVal = preorder[0];
+  const root = new TreeNode(rootVal);
+
+  const inorderIndex = inorder.indexOf(rootVal);
+
+  root.left = buildTree(preorder.slice(1, inorderIndex + 1), inorder.slice(0, inorderIndex));
+  root.right = buildTree(preorder.slice(inorderIndex + 1), inorder.slice(inorderIndex + 1));
+
+  return root;
+}
