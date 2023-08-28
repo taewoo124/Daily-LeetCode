7d8d8c071be9019d1b72782037860f040cc9f1b7
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-08-28 16:35:32 +0900

    Time: 66 ms (79.66%), Space: 44.8 MB (76.87%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.ts
@@ -0,0 +1,17 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  const result: number[][] = [];
+
+  for (let interval of intervals) {
+    if (interval[1] >= newInterval[0] && interval[0] <= newInterval[1]) {
+      newInterval = [Math.min(interval[0], newInterval[0]), Math.max(interval[1], newInterval[1])];
+    } else {
+      result.push(interval);
+    }
+  }
+
+  result.push(newInterval);
+  result.sort((a, b) => a[0] - b[0]);
+
+  return result;
+}
+
