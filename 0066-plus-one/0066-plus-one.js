b420d4cf76b4ac4f01c8f983e1e5a4f3d99b2d04
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-09-20 23:56:44 +0900

    Time: 55 ms (39.20%), Space: 41.7 MB (66.57%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  let carry = 1;
+  
+  for (let i = digits.length - 1; i >= 0; i--) {
+    const sum = digits[i] + carry;
+    digits[i] = sum % 10;
+    carry = Math.floor(sum / 10);
+  }
+  
+  if (carry) {
+    digits.unshift(carry);
+  }
+  
+  return digits;
+};
