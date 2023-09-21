8170043e291d24e3edf1e6c93444b20ea736ec8d
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-09-21 20:39:25 +0900

    Time: 63 ms (52.59%), Space: 45.6 MB (92.53%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -10,11 +10,10 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root, min = null, max = null) {
-    if (!root) return true;
-
-    if (min !== null && root.val <= min) return false;
-    if (max !== null && root.val >= max) return false;
-
-    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+var isValidBST = function(root, min = -Infinity, max = Infinity) {
+  if (!root) return true;
+  
+  if (root.val <= min || root.val >= max) return false;
+  
+  return isValidBST(root.right, root.val, max) && isValidBST(root.left, min, root.val);
 };
