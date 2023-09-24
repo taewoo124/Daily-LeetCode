a4b3da5c168ad002c475a7f77766266440f8e298
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-09-24 20:50:31 +0900

    Time: 59 ms (71.84%), Space: 45.1 MB (88.31%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -11,19 +11,20 @@
  * @return {number}
  */
 var diameterOfBinaryTree = function(root) {
-  let diameter = 0;
+    let result = 0;
+    
+    function depth(node) {
+      if (!node) return 0;
 
-  function depth(node) {
-    if (!node) return 0;
+      const leftDepth = depth(node.left);
+      const rightDepth = depth(node.right);
 
-    let leftDepth = depth(node.left);
-    let rightDepth = depth(node.right);
+      result = Math.max(result, leftDepth + rightDepth);
 
-    diameter = Math.max(diameter, leftDepth + rightDepth);
+      return Math.max(leftDepth, rightDepth) + 1;
+    }
 
-    return Math.max(leftDepth, rightDepth) + 1;
-  }
-
-  depth(root);
-  return diameter;
+    depth(root);
+  
+    return result;
 };
