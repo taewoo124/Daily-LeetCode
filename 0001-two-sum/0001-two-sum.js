259dd1c591dc85c09d11405accbff29e06b05c8f
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-10-03 16:21:09 +0900

    Time: 84 ms (41.43%), Space: 42.3 MB (69.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,17 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = new Map();
-    
-    for (let i = 0; i < nums.length; i += 1) {
-        const complement = target - nums[i];
-        
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-        }
-        
-        map.set(nums[i], i);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+
+        return [i, j];
+      }
     }
-    
-    // return [];
+  }
+
+  return [];
 };
+
