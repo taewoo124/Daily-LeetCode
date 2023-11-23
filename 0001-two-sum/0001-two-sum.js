baa5596554f1b43ffd4e599396c563fedf78d3ef
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-11-23 17:01:23 +0900

    Time: 87 ms (34.86%), Space: 42.7 MB (43.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,19 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
   const result = [];
-  
-  while (result.length !== 2) {
-    if (left === right) {
-      left++
-      right = nums.length - 1;
-    } else if (nums[left] + nums[right] === target) {
-      result.push(left, right);
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result.push(i, j);
+      }
     }
-    
-    right--;
   }
   
   return result;
