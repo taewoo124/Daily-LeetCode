ce875c31015ef28ee7d86daf410a5dd8b6a2a9cf
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-11-27 20:30:01 +0900

    Time: 53 ms (47.28%), Space: 41.7 MB (93.93%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var minimumSum = function(num) {
-  let digits = num.toString().split('');
+  const digits = num.toString().split('');
 
   digits.sort();
 
-  let new1 = parseInt(digits[0] + digits[2]);
-  let new2 = parseInt(digits[1] + digits[3]);
+  const new1 = parseInt(digits[0] + digits[2]);
+  const new2 = parseInt(digits[1] + digits[3]);
 
   return new1 + new2;
 };
