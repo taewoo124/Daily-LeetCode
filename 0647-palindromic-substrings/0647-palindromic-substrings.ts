be83adb8f18bd5ff3552df2ba3adb4da300197df
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-12-03 14:22:21 +0900

    Time: 495 ms (5.23%), Space: 94 MB (5.23%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.ts
+++ 0647-palindromic-substrings/0647-palindromic-substrings.ts
@@ -4,18 +4,14 @@ function countSubstrings(s: string): number {
   for (let i = 0; i < s.length; i++) {
     for (let j = i + 1; j < s.length + 1; j++) {
       const palindromString = s.slice(i, j);
-      if (checkPalindrom(palindromString)) {
-        result.push(palindromString);
-      }
+
+      if (checkPalindrom(palindromString)) result.push(palindromString);
     }
   }
   
-  return result.length
+  return result.length;
 };
 
-// palindrom 을 체크하는 함수를 만든다..
-// 문자열을 1부터 늘려가며 넣는다.
-//
 
 function checkPalindrom (str: string): boolean {
   let left: number = 0;
