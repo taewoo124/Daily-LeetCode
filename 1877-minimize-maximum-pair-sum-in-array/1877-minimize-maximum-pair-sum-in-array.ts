d2fc6038a906d676dc0b98e2c4e2426c7a417ffe
김태우 <121784425+taewoo124@users.noreply.github.com>
2023-12-04 13:13:19 +0900

    Time: 287 ms (64.98%), Space: 61.7 MB (10.71%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.ts
@@ -0,0 +1,18 @@
+function minPairSum(nums: number[]): number {
+  const sortedArray: number[] = [...nums].sort((a, b) => a- b);
+  const array: number[] = [];
+  
+  let start: number = 0;
+  let end: number = nums.length - 1;
+  const mid: number = (start + end) / 2;
+  
+  while (start <= mid) {
+    array.push(sortedArray[start] + sortedArray[end]);
+    start++;
+    end--;
+  }
+  
+  const result = Math.max(...array);
+  
+  return result;
+};
