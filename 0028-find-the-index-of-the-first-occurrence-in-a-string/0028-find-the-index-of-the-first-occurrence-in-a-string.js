73d2da89b1943df09f6e3c0713e1103ffc24e3f1
김태우 <121784425+taewoo124@users.noreply.github.com>
2024-02-15 13:02:10 +0900

    Time: 58 ms (32.78%), Space: 48.8 MB (24.77%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,14 +4,5 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-  for (let i = 0; i <= haystack.length - needle.length; i++) {
-    let result = "";
-    for (let j = i; j < i + needle.length; j++) {
-      result += haystack[j]
-    }
-    
-    if (result === needle) return i;
-  }
-  
-  return -1;
+  return haystack.indexOf(needle);
 };
